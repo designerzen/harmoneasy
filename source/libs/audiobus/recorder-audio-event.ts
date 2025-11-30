ac96fdc635485317a04905ea725da47ce302471f
zen <designerzen@gmail.com>
2025-11-30 01:01:54 +0000

    feat: Recorder of Events

---
+++ source/libs/audiobus/recorder-audio-event.ts
@@ -0,0 +1,27 @@
+import AudioEvent from "./audio-event.ts"
+
+export class RecorderAudioEvent {
+    
+    events:AudioEvent[] = []
+    
+    constructor(){
+        this.events = []
+    }
+
+    addEvents( events:AudioEvent[] ){
+        this.events.push(...events)
+        return this.events
+    }
+
+    addEvent( event:AudioEvent){
+        this.events.push(event)
+    }
+
+    clear(){
+        this.events.length = 0
+    }
+
+    exportData(){
+        return this.events
+    }
+}
