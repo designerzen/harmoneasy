eba09649bd64e74796354c6df3ab0b1795a8838f
zen <designerzen@gmail.com>
2025-12-03 18:30:52 +0000

    fix: AudioTool settings

--- source/libs/audiotool/audio-tool-settings.js
+++
@@ -1,6 +0,0 @@
-// LocalStorage keys
-export const STORAGE_KEYS = {
-  PAT_TOKEN: 'audiotool_nexus_pat_token',
-  PROJECT_URL: 'audiotool_nexus_project_url',
-  LOOP_DURATION:3000
-}
---
+++ source/libs/audiotool/audio-tool-settings.ts
@@ -0,0 +1,6 @@
+// LocalStorage keys
+export const AUDIOTOOL_STORAGE_KEYS = {
+  PAT_TOKEN: 'at_pat_sFoMCeBSURZQA8YuWHWKYtpYviWgd4fCVCwPKqxnjmA',
+  PROJECT_URL: 'https://beta.audiotool.com/studio?project=fe824261-7d93-4a75-95ec-3603d5891546',
+  LOOP_DURATION:3000
+}
