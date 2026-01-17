a9563cda9266abb6e95dc8556d17dc1deee3f225
zen <designerzen@gmail.com>
2026-01-17 21:16:57 +0000

    update: refactor of midi device

---
+++ source/libs/audiobus/midi/midi-channels.ts
@@ -0,0 +1,38 @@
+// All possible MIDI1.0 Channels
+export const ALL_MIDI_CHANNELS = [
+	1, 
+	2, 
+	3, 
+	4, 
+	5, 
+	6, 
+	7, 
+	8, 
+	9, 
+	10, 
+	11, 
+	12, 
+	13, 
+	14, 
+	15, 
+	16
+]
+
+export const ALL_MIDI_CHANNEL_LABELS = [
+	"Channel 1", 
+	"Channel 2", 
+	"Channel 3", 
+	"Channel 4", 
+	"Channel 5", 
+	"Channel 6", 
+	"Channel 7", 
+	"Channel 8", 
+	"Channel 9", 
+	"Channel 10 - Percussion", 
+	"Channel 11", 
+	"Channel 12", 
+	"Channel 13", 
+	"Channel 14", 
+	"Channel 15", 
+	"Channel 16"
+]
