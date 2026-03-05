e629be31d7bda7e5d2e6e3658be3c1bb749ec08c
zen <designerzen@gmail.com>
2026-03-05 21:09:34 +0000

    fix: Github CI

--- app/harmoneasy/vite.config.web.js
+++ app/harmoneasy/vite.config.web.js
@@ -14,7 +14,7 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      external: ['url', 'path', /\.node$/]
+      external: ['url', 'path', /\.node$/, 'netronome', 'audiobus', 'audiotool', 'flodjs', 'midi-ble', 'opendaw', 'pink-trombone', 'pitfalls']
     }
   },
   test: {
