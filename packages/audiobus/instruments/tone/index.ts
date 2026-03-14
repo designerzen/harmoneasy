f4ff7df6ee1eb6ed9e541411c140e935fe61a52a
zen <designerzen@gmail.com>
2026-03-14 14:04:48 +0000

    fix: remove static exports from tone instruments to enable proper lazy loading

--- packages/audiobus/instruments/tone/index.ts
+++ packages/audiobus/instruments/tone/index.ts
@@ -1,7 +1,3 @@
 // Tone.js Instruments
-
-export { default as ToneSynth } from "./tone-synth.ts"
-export { default as ToneMonoSynth } from "./tone-mono-synth.ts"
-export { default as TonePluckString } from "./tone-pluck-string.ts"
-export { default as ToneSampler } from "./tone-sampler.ts"
-export { default as ToneFMSynth } from "./tone-fm-synth.ts"
+// Note: These are lazy-loaded via instrument-types.ts
+// Do not export statically to avoid bundling conflicts with dynamic imports
